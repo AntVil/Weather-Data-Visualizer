70318ee810aac6e3564c9145083e1f05841d6a4d
AntVil <56440776+AntVil@users.noreply.github.com>
2021-05-10 16:56:18 +0200

    moved plot

--- src/main.py
+++ src/main.py
@@ -1,17 +1,1 @@
 # this file is the entry point for the project
-
-import cartopy
-from matplotlib import pyplot as plt
-
-ax = plt.axes(projection = cartopy.crs.PlateCarree())
-
-ax.add_feature(cartopy.feature.LAND)
-ax.add_feature(cartopy.feature.OCEAN)
-ax.add_feature(cartopy.feature.COASTLINE)
-ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
-ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
-ax.add_feature(cartopy.feature.RIVERS)
-
-ax.set_extent([-20, 60, -40, 40])
-
-plt.show()
---
+++ src/plot.py
@@ -0,0 +1,16 @@
+# this file contains all components needed to plot data on a map
+import cartopy
+from matplotlib import pyplot as plt
+
+ax = plt.axes(projection = cartopy.crs.PlateCarree())
+
+ax.add_feature(cartopy.feature.LAND)
+ax.add_feature(cartopy.feature.OCEAN)
+ax.add_feature(cartopy.feature.COASTLINE)
+ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
+ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
+ax.add_feature(cartopy.feature.RIVERS)
+
+ax.set_extent([-20, 60, -40, 40])
+
+plt.show()
