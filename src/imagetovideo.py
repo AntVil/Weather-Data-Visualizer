8c7d96dbdbe454a289cfe8cc625b5e01c81cbbb2
AntVil <56440776+AntVil@users.noreply.github.com>
2021-05-12 10:38:36 +0200

    added test images +changed to mp4 format

--- src/imagetovideo.py
+++ src/imagetovideo.py
@@ -3,14 +3,19 @@ import cv2
 import os
 
 
-def image_to_video(*image_paths):
+# constants
+IMAGES_FOLDER = os.path.join(os.path.dirname(__file__), "data", "images")
+VIDEOS_FOLDER = os.path.join(os.path.dirname(__file__), "data", "videos")
+
+
+def image_to_video(image_paths, save_to=os.path.join(VIDEOS_FOLDER, "_test_video.mp4")):
     """
     merges all given images to a video
     """
 
     size = cv2.imread(image_paths[0]).shape[1::-1]
 
-    out = cv2.VideoWriter("video.avi", cv2.VideoWriter_fourcc(*"DIVX"), 1, size)
+    out = cv2.VideoWriter(save_to, cv2.VideoWriter_fourcc(*"mp4v"), 12, size)
 
     for image_path in image_paths:
         out.write(cv2.imread(image_path))
@@ -20,5 +25,6 @@ def image_to_video(*image_paths):
 
 if __name__ == "__main__":
     image_to_video(
-        os.path.join(os.path.dirname(__file__), "data", "images", "image.jpg")
+        [os.path.join(IMAGES_FOLDER, f"_test_image_{i}.png") for i in range(25)],
+        os.path.join(VIDEOS_FOLDER, "_test_video.mp4")
     )
