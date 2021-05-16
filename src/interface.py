0900c8d84b8bb798106daeb64aca2e7f033cdb5d
Jandorid <73158569+Jandorid@users.noreply.github.com>
2021-05-16 17:28:20 +0200

    Update interface.py

--- src/interface.py
+++ src/interface.py
@@ -1,7 +1,7 @@
 import cartopy
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
-from matplotlib.figure import Figure
-
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 import sys
 import tkinter as Tk
 
@@ -12,22 +12,32 @@ class weather_data_interface:
         self.root.wm_title("Weather Data")
         
         #Cartopy TKinter integration
-        self.fig = Figure(figsize=(10,6), dpi=100)
-        ax = self.fig.add_axes([0.01, 0.01, 0.98, 0.98],projection = cartopy.crs.PlateCarree())
-        ax.add_feature(cartopy.feature.LAND)
-        ax.add_feature(cartopy.feature.OCEAN)
-        ax.add_feature(cartopy.feature.COASTLINE)
-        ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
-        ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
-        ax.add_feature(cartopy.feature.RIVERS)
+        
+        self.fig, self.ax = plt.subplots(figsize=(10, 6))
+        
+        self.ax.axis('off')
+        self.ax = plt.axes(projection=ccrs.PlateCarree())
+        
 
-        ax.set_extent([5, 16, 57, 45])
+        self.ax.plot(10, 50, 'bo', markersize=7, transform=ccrs.Geodetic())
+        self.ax.text(10, 51, 'test', transform=ccrs.Geodetic())
+
+        self.ax.add_feature(cartopy.feature.LAND)
+        self.ax.add_feature(cartopy.feature.OCEAN)
+        self.ax.add_feature(cartopy.feature.COASTLINE)
+        self.ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
+        self.ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
+        self.ax.add_feature(cartopy.feature.RIVERS)
+
+        self.ax.set_extent([5, 16, 57, 45])
+
 
         #Drawing canvas and toolbar for navigation
+
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         self.toolbar = NavigationToolbar2Tk(self.canvas, self.root)
         self.toolbar.update()
-
+        
 
         self.canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
