7e6a8233b634585f20aa956ba6d7cb9b2cf57c4c
AntVil <56440776+AntVil@users.noreply.github.com>
2021-05-17 16:21:49 +0200

    interpolated plotting

--- src/plot.py
+++ src/plot.py
@@ -1,28 +1,43 @@
 # this file contains all components needed to plot data on a map
 import cartopy
 from matplotlib import pyplot as plt
+from matplotlib import tri
 from dwd import get_dwd_DataFrames
+import numpy as np
 
-ax = plt.axes(projection = cartopy.crs.PlateCarree())
 
-ax.add_feature(cartopy.feature.LAND)
-ax.add_feature(cartopy.feature.OCEAN)
-ax.add_feature(cartopy.feature.COASTLINE)
-ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
-ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
-ax.add_feature(cartopy.feature.RIVERS)
+if __name__ == "__main__":
+    # plotting map of germany
+    ax = plt.axes(projection = cartopy.crs.PlateCarree())
+    ax.add_feature(cartopy.feature.LAND)
+    ax.add_feature(cartopy.feature.OCEAN)
+    ax.add_feature(cartopy.feature.COASTLINE)
+    ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
+    ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
+    ax.add_feature(cartopy.feature.RIVERS)
+    ax.set_extent([5, 16, 56, 46])
 
-ax.set_extent([5, 16, 56, 46])
+    # collecting data from dwd
+    x = []
+    y = []
+    z = []
+    for df in get_dwd_DataFrames():
+        lat = float(df.LON[0])
+        lon = float(df.LAT[0])
+        temperature = float(df.TEMPERATURE[0])
+        if temperature > -50 and temperature < 100:
+            x.append(lat)
+            y.append(lon)
+            z.append(temperature)
 
-x = []
-y = []
-z = []
+    # creating grid
+    xi = np.arange(5, 16, 0.1)
+    yi = np.arange(46, 56, 0.1)
 
-for df in get_dwd_DataFrames():
-    x.append(float(df.LON[0]))
-    y.append(float(df.LAT[0]))
-    z.append(float(df.TEMPERATURE[0]))
+    # interpolating vaues on grid
+    interpolator = tri.LinearTriInterpolator(tri.Triangulation(x, y), z)
+    zi = interpolator(*np.meshgrid(xi, yi))
 
-ax.contourf(x, y, z)
-
-plt.show()
+    # plotting
+    ax.contourf(xi, yi, zi, levels=14)
+    plt.show()
