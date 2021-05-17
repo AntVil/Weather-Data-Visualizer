fde27ec64dea84df5f19850726dad773f2d042a2
AntVil <56440776+AntVil@users.noreply.github.com>
2021-05-17 09:16:58 +0200

    plotting stations

--- src/plot.py
+++ src/plot.py
@@ -1,6 +1,7 @@
 # this file contains all components needed to plot data on a map
 import cartopy
 from matplotlib import pyplot as plt
+from dwd import get_dwd_DataFrames
 
 ax = plt.axes(projection = cartopy.crs.PlateCarree())
 
@@ -11,6 +12,17 @@ ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
 ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
 ax.add_feature(cartopy.feature.RIVERS)
 
-ax.set_extent([5, 16, 57, 45])
+ax.set_extent([5, 16, 56, 46])
+
+x = []
+y = []
+z = []
+
+for df in get_dwd_DataFrames():
+    x.append(float(df.LON[0]))
+    y.append(float(df.LAT[0]))
+    z.append(float(df.TEMPERATURE[0]))
+
+ax.contourf(x, y, z)
 
 plt.show()
